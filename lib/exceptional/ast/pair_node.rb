97a3e019c450fa9fde350cebaccb96ff3cd5e2d1
Guillaume Malette <guillaume@jadedpixel.com>
2016-01-27 10:34:24 -0500

    Typo

--- lib/exceptional/ast/pair_node.rb
+++ lib/exceptional/ast/pair_node.rb
@@ -2,7 +2,7 @@ module Exceptional
   module Ast
     PairNode = Struct.new(:binding_name, :value) do
       def initialize(binding_name:, value:)
-        self.target = target
+        self.binding_name = binding_name
         self.value = value
       end
     end
